453ddc8b032d45d09979944f0991cab5588aba0b
Desiqvel <daniel.siqveland@hotmail.com>
2019-08-30 16:52:56 +0200

    fiksa litt men funker ikke helt

--- sublister.py
+++ sublister.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sublist3r 
-import dig
+#import dig
 import sys
+import os
 
 dName = sys.argv[1]
 
@@ -10,4 +11,12 @@ print ("Starting to print from " + dName)
 
 subdomains = sublist3r.main(dName, 40, dName + 'sub.txt', ports= None, silent=False, verbose= False, enable_bruteforce= False, engines=None)
 
-ipddr = dig -f dName.txt +short | grep -P '^(?:(?![a-z]).)*$' > dname_ipaddr.txt
+#cmd='dig -f dName.txt +short | grep -P '^(?:(?![a-z]).)*$' > dname_ipaddr.txt'
+
+#myCmd = 'dig -f dNamesub.txt +short > dNameipaddr.txt'
+
+myCmd = ("dig -f %Ssub.txt +short > %Iipaddr.txt" % (dName, dName))
+
+#ipaddrun = dig -f dNamesub.txt +short 
+for i in myCmd:
+  print(i)
