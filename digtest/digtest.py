ad1fae8257b3f070870a688c571afc8e3cde3021
Daniel Krohn <33431028+Desiqvel@users.noreply.github.com>
2019-09-02 16:36:47 +0200

    endret kommentering

--- digtest/digtest.py
+++ digtest/digtest.py
@@ -8,8 +8,8 @@ import os
 #dName = sys.argv[1]
 line = ("elasticsearch5.anbefaling.nrk.no")
 dName = ("testNRK")
-#myCmd = ("dig elasticsearch5.anbefaling.nrk.no +short | grep -P '^(?:(?![a-z]).)*$' > test2.txt")
-myCmd = ("dig %s +short | grep -P '^(?:(?![a-z]).)*$' >> %sipaddr.txt" % (line, dName))
+myCmd = ("dig elasticsearch5.anbefaling.nrk.no +short | grep -P '^(?:(?![a-z]).)*$' > test2.txt")
+#myCmd = ("dig %s +short | grep -P '^(?:(?![a-z]).)*$' >> %sipaddr.txt" % (line, dName))
 
 os.system(myCmd)
 
