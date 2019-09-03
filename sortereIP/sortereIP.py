c99c967cd0f046528b694fb6f0dba0b25fa833ec
Desiqvel <daniel.siqveland@hotmail.com>
2019-09-03 22:00:55 +0200

    Backup

--- sortereIP/sortereIP.py
+++ sortereIP/sortereIP.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import sublist3r
+import sys
+import os
+import subprocess
+
+
 array = [['IP', 'SUB']]
 
 
@@ -11,16 +17,27 @@ with open('nrk.noipaddr.txt') as IPdom:
 i = 1
 for i in array:
 	ip = i.pop(0)
-	ip = ip.rstrip()
+	ip = ip.strip()
 	print(ip)
 
-ip.sort(key=lambda s: map(int, s.split('.')))
+#ip.sort(key=lambda s: map(int, s.split('.')))
 
-outputFile = open(sortereIP.txt, "w")
+#from collections import namedtuple
+#ip_type = namedtuple("IP", 'g1 g2 g3 g4 port')
+
+#def to_ip(string) -> ip_type:
+#    g1, g2, g3, g4 = [int(g) for g in string.split('.')]
+#    return ip_type(g1, g2, g3, g4 )
+
+
+#print(sorted(array, key=to_ip))
+
+#print (ip)
+outputFile = open('usortereIP.txt', "w")
 
 for s in ip:
 	outputFile.write(s+"\n")
 
-outputFile.close()
-text_file.close()
+#outputFile.close()
+IPdom.close()
 
