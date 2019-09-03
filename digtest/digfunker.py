4f9f98a92a9347c14172ebe48747a2dc229bbff9
Desiqvel <daniel.siqveland@hotmail.com>
2019-09-03 18:44:49 +0200

    tryed to add list with only IP adresses, didnt work... WIP

--- digtest/digfunker.py
+++ digtest/digfunker.py
@@ -9,6 +9,9 @@ dName = sys.argv[1]
 
 ofName = dName + "output.txt"
 outputFile = open(ofName, "w")
+iplist = dName + "IPout.txt"
+outputIP = open(iplist, "w")
+
 with open(dName + 'sub.txt') as text_file:
 	for domainName in text_file:
 		domainName = domainName.rstrip()
@@ -22,7 +25,11 @@ with open(dName + 'sub.txt') as text_file:
 				outputFile.write(line+"|"+domainName+"\n")
 		else:
 			outputFile.write(ip[0]+"|"+domainName+"\n")
+	#planen her er å legge den i en array og sjekke om den allerede finnes
+	#i arrayen, hvis ikke legger man den inn, deretter skriver til en fil.
+		outputIP.write(ip[0]+"\n")
 
 
 outputFile.close()
+outputIP.close()
 text_file.close()
