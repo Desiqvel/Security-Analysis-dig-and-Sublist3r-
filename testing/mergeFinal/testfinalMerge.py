78e3564ace91b1ecf87ca5f61707172055cb8e6f
Desiqvel <daniel.siqveland@hotmail.com>
2020-01-08 13:24:36 +0000

    backup from vm

--- testing/mergeFinal/testfinalMerge.py
+++ testing/mergeFinal/testfinalMerge.py
@@ -9,20 +9,29 @@ import numpy
 dName = 'vg.no'
 
 #arraymerge = []
-outfilemerge = open(dName + '.merged.txt', "w")   	# Writes final merge to this file
+#outfilemerge = open(dName + '.merged.txt', "w")   	# Writes final merge to this file
 
+arraymerge = []
+count = -1
 
 with open(dName + '.sortNoDupIP.txt', "r") as sortip: 	# Opens sorted IP-addresses
+	arraymerge.append([])
+	count = count+1
 	ipsub = open(dName + '.IP_SUB.txt', "r")	# Opens IP-addresses with sub-domains
 	for ip_sub in ipsub:				# Goes in a loop with all sub-domains
-		ip_sub = ipsub.rstrip()			# Strips whitespace Right
+#		ip_sub = ipsub.rstrip()			# Strips whitespace Right
 		ip_sub = ip_sub.split('|')		# Splits IP_SUB in IP-addresses and sub-domains
 #		sub = sub[1]				# 
-		#print (sub)
-		temp = ()				# Creates a temp to hold strings with sub-domains
-		if sortip == sub[0]:			# If IP-address matches IP-address in IP_SUB
-			temp.append(sub[1])		# Add string with sub-domain to temp
-	outfilemerge.write(sortip+'|'+temp)		# Writes to file with format (IP|SUB1,SUB2,SUBx)
+		arraymerge[count].append(sortip)
+		#temp = ()				# Creates a temp to hold strings with sub-domains
+		if sortip == ip_sub[0]:			# If IP-address matches IP-address in IP_SUB
+			arraymerge[count].append(ip_sub[1])		# Add string with sub-domain to temp
+
+#old	outfilemerge.write(sortip+'|'+temp)		# Writes to file with format (IP|SUB1,SUB2,SUBx)
+
+with open(dName + '.merged.txt', "w") as f:   	# Writes final merge to this file
+	for item in arraymerge:
+		f.write("%s\n" % item)		# Writes to file with format (IP|SUB1,SUB2,SUBx)
 	
 
 
@@ -33,6 +42,6 @@ with open(dName + '.sortNoDupIP.txt', "r") as sortip: 	# Opens sorted IP-address
 #				if line not in lines_seen: 
 					#lines_seen.add(line)
 	#arraymerge.append()
-outfilemerge.close()
+f.close()
 
 
